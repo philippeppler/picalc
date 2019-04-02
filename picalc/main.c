11f2a37c610adc6a515edb51d2f333d4c4c567a3
philippeppler <philipp.eppler@gmx.ch>
2019-04-02 19:24:18 +0200

    Prio gewechselt, Task umbenannt

--- picalc/main.c
+++ picalc/main.c
@@ -18,6 +18,7 @@
 #include "queue.h"
 #include "event_groups.h"
 #include "stack_macros.h"
+//#include "math.h"
 
 #include "mem_check.h"
 
@@ -28,9 +29,9 @@
 
 
 extern void vApplicationIdleHook( void );
-void vDisplay(void *pvParameters);
+void vGUI(void *pvParameters);
 
-TaskHandle_t DisplayTask;
+TaskHandle_t GUITask;
 
 void vApplicationIdleHook( void )
 {	
@@ -39,35 +40,26 @@ void vApplicationIdleHook( void )
 
 int main(void)
 {
-   // resetReason_t reason = getResetReason();
-
 	vInitClock();
 	vInitDisplay();
 	
-	xTaskCreate( vDisplay, (const char *) "DisplayTask", configMINIMAL_STACK_SIZE, NULL, 1, &DisplayTask);
+	xTaskCreate( vGUI, (const char *) "GUITask", configMINIMAL_STACK_SIZE, NULL, 2, &GUITask);
 
-	vDisplayClear();
-	vDisplayWriteStringAtPos(0,0,"PI Calculator");
-	vDisplayWriteStringAtPos(1,0,"Philipp Eppler");
-	vDisplayWriteStringAtPos(2,0,"Pi: 3.1415xxx");
-	vDisplayWriteStringAtPos(3,0,"Zeit: xxxxxxms");
 	vTaskStartScheduler();
 	return 0;
 }
 
-void vDisplay(void *pvParameters) {
+void vGUI(void *pvParameters) {
 	(void) pvParameters;
 	PORTF.DIRSET = PIN0_bm; /*LED1*/
 	PORTF.OUT = 0x01;
 	for(;;) {
-// 		uint32_t stack = get_mem_unused();
-// 		uint32_t heap = xPortGetFreeHeapSize();
-// 		uint32_t taskStack = uxTaskGetStackHighWaterMark(ledTask);
-// 		vDisplayClear();
-// 		vDisplayWriteStringAtPos(0,0,"Stack: %d", stack);
-// 		vDisplayWriteStringAtPos(1,0,"Heap: %d", heap);
-// 		vDisplayWriteStringAtPos(2,0,"TaskStack: %d", taskStack);
-// 		vDisplayWriteStringAtPos(3,0,"FreeSpace: %d", stack+heap);
+		vDisplayClear();
+		vDisplayWriteStringAtPos(0,0,"PI Calculator");
+		vDisplayWriteStringAtPos(1,0,"Philipp Eppler");
+		vDisplayWriteStringAtPos(2,0,"Pi: 3.1415xxx");
+		vDisplayWriteStringAtPos(3,0,"Zeit: xxxxxxms");
+		
 		PORTF.OUTTGL = 0x01;				
 		vTaskDelay(500 / portTICK_RATE_MS);
 	}
