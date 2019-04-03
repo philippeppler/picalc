23105287dcd9737c6cdef607b08b6d82410bdb07
philippeppler <philipp.eppler@gmx.ch>
2019-04-03 18:50:12 +0200

    buttons, Eventgroup
    next:
    Eventgroups müssen das korrekte bewirken

--- picalc/main.c
+++ picalc/main.c
@@ -31,7 +31,14 @@
 #include "NHD0420Driver.h"
 #include "ButtonHandler.h"
 
-#define ITERATIONS 100000
+#define ITERATIONS 1000000
+
+// EventGroup
+#define STARTCALC	1<<0
+#define STOPCALC	1<<1
+#define RESETCALC	1<<2
+#define FINISHCALC	1<<3
+EventGroupHandle_t egPiStates;
 
 extern void vApplicationIdleHook( void );
 void vGUI(void *pvParameters);
@@ -41,7 +48,6 @@ void vCalc(void *pvParameters);
 TaskHandle_t GUITask;
 
 double dPi4; 
-long i;
 
 
 void vApplicationIdleHook( void )
@@ -53,6 +59,7 @@ int main(void)
 {
 	vInitClock();
 	vInitDisplay();
+	egPiStates = xEventGroupCreate();
 	
 	xTaskCreate( vButton, (const char *) "Button", configMINIMAL_STACK_SIZE, NULL, 3, NULL);
 	xTaskCreate( vGUI, (const char *) "GUITask", configMINIMAL_STACK_SIZE, NULL, 2, &GUITask);
@@ -66,17 +73,17 @@ void vGUI(void *pvParameters) {
 	char Pi[10] = "";			
 	char Iter[15] = "";
 	for(;;) {
-
+		
+		xEventGroupClearBits(egPiStates, FINISHCALC);
 		sprintf(Pi, "%f", 4*dPi4);
 		sprintf(Iter, "%ld", i);
-		
 		vDisplayClear();
 		vDisplayWriteStringAtPos(0,0,"PI Calculator");
 		vDisplayWriteStringAtPos(1,0,"%s", Iter);
 		vDisplayWriteStringAtPos(2,0,"Pi: %s", Pi);
 		vDisplayWriteStringAtPos(3,0,"Zeit: xxxxxxms");
+		xEventGroupSetBits(egPiStates, FINISHCALC);
 		
-
 		vTaskDelay(500 / portTICK_RATE_MS);
 	}
 }
@@ -87,15 +94,18 @@ void vButton(void *pvParameters) {
 	while (1) {
 		updateButtons();
 		if (getButtonPress(BUTTON1) == SHORT_PRESSED) {
-
+			xEventGroupSetBits(egPiStates, STARTCALC);
+			xEventGroupClearBits(egPiStates, STOPCALC);
 		}
 
 		if (getButtonPress(BUTTON2) == SHORT_PRESSED) {
-
+			xEventGroupSetBits(egPiStates, STOPCALC);
+			xEventGroupClearBits(egPiStates, STARTCALC);
 		}
 		
 		if (getButtonPress(BUTTON3) == SHORT_PRESSED) {
-
+			xEventGroupSetBits(egPiStates, RESETCALC);
+			
 		}		
 
 		if (getButtonPress(BUTTON4) == SHORT_PRESSED) {
@@ -108,9 +118,32 @@ void vButton(void *pvParameters) {
 
 void vCalc(void *pvParameters) {
 	dPi4 = 1;
+	uint16_t calcstate = 0x0000;
 	
-	for(i = 0;i<ITERATIONS;i++) {
-		dPi4 = dPi4 - (1.0/(3+4*i)) + (1.0/(5+4*i));
+	for(long i = 0;;i++) {
+		
+		calcstate = xEventGroupGetBits(egPiStates);
+	//	xEventGroupWaitBits(egPiStates, STARTCALC, pdFALSE, pdFALSE, portMAX_DELAY);
+		
+		if (calcstate & FINISHCALC) {
+			if (calcstate & STARTCALC) {
+				dPi4 = dPi4 - (1.0/(3+4*i)) + (1.0/(5+4*i));
+			}
+		}
+		if (calcstate & STOPCALC) {
+			// do nothing
+		}
+		if (calcstate & RESETCALC) {
+			dPi4 = 1;
+			i = 0;
+			xEventGroupClearBits(egPiStates, RESETCALC);
+		}
+		
+		 
+		
+		
+		
+		
 		vTaskDelay(1/portTICK_RATE_MS);
 	}
 	
